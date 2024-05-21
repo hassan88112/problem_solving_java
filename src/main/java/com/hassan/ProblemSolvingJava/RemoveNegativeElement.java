f9274cb27000dd0751e1d91382c08ab28e11d812
hshalash <hshalash@arpuplus.com>
2024-05-21 14:34:33 +0300

    solve it with List instead of array

--- src/main/java/com/hassan/ProblemSolvingJava/RemoveNegativeElement.java
+++ src/main/java/com/hassan/ProblemSolvingJava/RemoveNegativeElement.java
@@ -1,11 +1,27 @@
 package com.hassan.ProblemSolvingJava;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RemoveNegativeElement {
 
 
 
-    public  void removeNegative(){
+    public static void main(String[] args) {
+        int[] arr = {3, -3, 45, 2, -19, -7, 56, 45};
+        int n = arr.length;
 
+        List<Integer> allNumbers=new ArrayList<>();
+        List<Integer> negativeNumbers=new ArrayList<>();
 
+        for (int i = 0 ;i < n; i++){
+            if (arr[i] > 0){
+                allNumbers.add(arr[i]);
+            }else {
+                negativeNumbers.add(arr[i]);
+            }
+        }
+        allNumbers.addAll(negativeNumbers);
+        System.out.println(allNumbers);
     }
 }
